c1667ac3982309bf09af9fa5e5220fb008473fe0
Linus Johansson <golfarenlj@gmail.com>
2025-01-02 13:29:11 +0000

    initialize React app with essential files and configurations

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <div>Pick & Pair</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
