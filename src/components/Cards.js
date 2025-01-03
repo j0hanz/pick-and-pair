d5c04500d6c59d2bc97a33dcec4a0581f3a2665f
Linus Johansson <golfarenlj@gmail.com>
2025-01-03 20:39:49 +0000

    Add modal display for game over and victory messages in Cards component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,17 +7,20 @@ import Score from './Score';
 import { shuffleCards } from '../utils/shuffleCards';
 import { useGameLogic } from '../hooks/useGameLogic';
 import { Row, Col, Button } from 'react-bootstrap';
+import Modal from './Modal';
 
-export default function Cards() {
+export default function Cards({ onRestart }) {
   const [cards, setCards] = useState(() => shuffleCards(initialCards));
   const [selectedCardIndex, setSelectedCardIndex] = useState(null);
   const [matchedPairs, setMatchedPairs] = useState(0);
   const previousIndex = useRef(null);
   const [resetTrigger, setResetTrigger] = useState(false);
   const [, startTransition] = useTransition();
-  const [timeLeft, setTimeLeft] = useState(120);
+  const [timeLeft, setTimeLeft] = useState(60);
   const [isGameOver, setIsGameOver] = useState(false);
   const [isInitialFlip, setIsInitialFlip] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
 
   const { resetGameWithTimer, handleCardSelection } = useGameLogic({
     cards,
@@ -37,8 +40,17 @@ export default function Cards() {
   useEffect(() => {
     if (matchedPairs === initialCards.length / 2) {
       setIsGameOver(true);
+      setModalMessage('Congratulations! You matched all cards!');
+      setShowModal(true);
     }
   }, [matchedPairs]);
+
+  useEffect(() => {
+    if (isGameOver && timeLeft === 0) {
+      setModalMessage('Time is up! Game over.');
+      setShowModal(true);
+    }
+  }, [isGameOver, timeLeft]);
 
   useEffect(() => {
     // Flip all cards initially
@@ -83,6 +95,13 @@ export default function Cards() {
           </Col>
         ))}
       </Row>
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        onRestart={onRestart}
+      >
+        {modalMessage}
+      </Modal>
     </div>
   );
 }
