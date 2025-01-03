298269abfea0ad038f5937029edba4e7af6480ad
Linus Johansson <golfarenlj@gmail.com>
2025-01-03 20:39:39 +0000

    Add game start toggle functionality to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import styles from './App.module.css';
 import Cards from './components/Cards';
 
 function App() {
+  const [isGameActive, setIsGameActive] = useState(false);
+
+  const toggleGame = () => setIsGameActive((prev) => !prev);
+
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
         <Container className={styles.container}>
-          <Cards />
+          {isGameActive ? (
+            <Cards onRestart={toggleGame} />
+          ) : (
+            <Button onClick={toggleGame} className={styles.button}>
+              Start Game
+            </Button>
+          )}
         </Container>
       </header>
     </div>
