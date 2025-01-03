8c907ae32c39b2b57fc58238ea4d314492172191
Linus Johansson <golfarenlj@gmail.com>
2025-01-03 15:58:38 +0000

    Refactor showFeedback function for improved readability and streamline handleRightAnswer and handleWrongAnswer exports

--- src/utils/feedbackHandler.js
+++ src/utils/feedbackHandler.js
@@ -1,8 +1,9 @@
 import '../feedback.css';
 
 const showFeedback = (type, message, duration = 3000, callback) => {
-  const existingToasts = document.querySelectorAll('.feedback-toast');
-  existingToasts.forEach((toast) => toast.remove());
+  document
+    .querySelectorAll('.feedback-toast')
+    .forEach((toast) => toast.remove());
 
   const feedback = document.createElement('div');
   feedback.className = `feedback-toast ${type}`;
@@ -15,10 +16,8 @@ const showFeedback = (type, message, duration = 3000, callback) => {
   }, duration);
 };
 
-export const handleRightAnswer = (callback) => {
+export const handleRightAnswer = (callback) =>
   showFeedback('correct', 'Correct Match!', 3000, callback);
-};
 
-export const handleWrongAnswer = (callback) => {
+export const handleWrongAnswer = (callback) =>
   showFeedback('wrong', 'Wrong Match!', 3000, callback);
-};
