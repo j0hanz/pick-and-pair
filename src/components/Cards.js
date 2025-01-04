a753d6db5b1acdd4895a0170f411e288a0fa75ea
Linus Johansson <golfarenlj@gmail.com>
2025-01-04 23:07:41 +0000

    Add Score and Timer components to Cards for enhanced game stats display

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Card from './Card';
 import { Row, Col } from 'react-bootstrap';
+import Score from './Score';
+import Timer from './Timer';
 import styles from '../App.module.css';
 
-export default function Cards({ cards, isInitialFlip, handleCardSelection }) {
+export default function Cards({
+  cards,
+  isInitialFlip,
+  handleCardSelection,
+  matchedPairs,
+  initialTime,
+  onTimeUp,
+}) {
   return (
-    <Row className={styles.row} noGutters>
+    <Row className="d-flex justify-content-between" noGutters={true}>
+      <div className={styles.stats}>
+        <Score matchedPairs={matchedPairs} />
+        <Timer initialTime={initialTime} onTimeUp={onTimeUp} />
+      </div>
       {cards.map((card, index) => (
         <Col xs={3} sm={3} md={3} lg={2} xl={2} key={index}>
           <Card
