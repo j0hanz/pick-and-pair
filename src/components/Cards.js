d14a999a3ed3624228b7de20764298bd424b8830
Linus Johansson <golfarenlj@gmail.com>
2025-01-05 11:23:34 +0000

    Add Attempts component to Cards for displaying current attempts

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,6 +3,7 @@ import Card from './Card';
 import { Row, Col } from 'react-bootstrap';
 import Score from './Score';
 import Timer from './Timer';
+import Attempts from './Attempts';
 import styles from '../App.module.css';
 
 export default function Cards({
@@ -12,12 +13,14 @@ export default function Cards({
   matchedPairs,
   initialTime,
   onTimeUp,
+  attempts,
 }) {
   return (
     <Row className={styles.row}>
       <div className={styles.stats}>
         <Score matchedPairs={matchedPairs} />
         <Timer initialTime={initialTime} onTimeUp={onTimeUp} />
+        <Attempts attempts={attempts} />
       </div>
       {cards.map((card, index) => (
         <Col xs={4} sm={4} md={3} lg={2} xl={2} key={index}>
