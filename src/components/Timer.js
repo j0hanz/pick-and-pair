1fce2348b642a52785f0386f60f1bb902aefdd97
Linus Johansson <golfarenlj@gmail.com>
2025-01-05 16:55:10 +0000

    Refactor Timer component to use external useCountdown hook and simplify code

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,31 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { TbClock } from 'react-icons/tb';
-
-// Custom hook for countdown
-function useCountdown(initialTime, onTimeUp) {
-  const [remainingTime, setRemainingTime] = useState(initialTime);
-
-  useEffect(() => {
-    // If time is up, call the onTimeUp callback
-    if (remainingTime <= 0) {
-      onTimeUp();
-      return;
-    }
-
-    // Decrement the remaining time every second
-    const timerId = setInterval(() => {
-      setRemainingTime((prevTime) => prevTime - 1);
-    }, 1000);
-
-    // Cleanup interval on component unmount or when remainingTime changes
-    return () => clearInterval(timerId);
-  }, [remainingTime, onTimeUp]);
-
-  return remainingTime;
-}
+import { useCountdown } from '../hooks/useCountdown';
 
 export default function Timer({ initialTime, onTimeUp }) {
-  // Use the custom countdown hook
+  // Use the useCountdown hook to get the remaining time
   const remainingTime = useCountdown(initialTime, onTimeUp);
 
   return (
