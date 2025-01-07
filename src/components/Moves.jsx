336032942780d2a53c2bc54b578e98d1091975fc
Linus Johansson <golfarenlj@gmail.com>
2025-01-07 23:59:42 +0000

    Refactor Moves component to use memo for performance optimization

--- src/components/Moves.jsx
+++ src/components/Moves.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { HiOutlineXMark } from 'react-icons/hi2';
 import styles from './styles/Moves.module.css';
 
-export default function Moves({ moves }) {
+const Moves = memo(({ moves }) => {
   return (
     <div className={styles.moves}>
       <HiOutlineXMark className={`${styles.statsIcon} me-1`} /> {moves}
     </div>
   );
-}
+});
+
+Moves.displayName = 'Moves';
+export default Moves;
