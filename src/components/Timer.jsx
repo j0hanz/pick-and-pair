5e1de38e51518bf097dd924379b7f1ec141a2b8d
Linus Johansson <golfarenlj@gmail.com>
2025-01-08 11:49:54 +0000

    Refactor Timer component to use useTimer hook for elapsed time calculation

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,14 +1,14 @@
 import React, { memo } from 'react';
 import { HiOutlineClock } from 'react-icons/hi2';
-import { useCountdown } from '../hooks/useCountdown';
+import { useTimer } from '../hooks/useTimer';
 import styles from './styles/Timer.module.css';
 
-const Timer = memo(({ initialTime, onTimeUp }) => {
-  const remainingTime = useCountdown(initialTime, onTimeUp);
+const Timer = memo(() => {
+  const elapsedTime = useTimer();
 
   return (
     <div className={styles.timer}>
-      <HiOutlineClock className={`${styles.statsIcon} me-2`} /> {remainingTime}
+      <HiOutlineClock className={`${styles.statsIcon} me-2`} /> {elapsedTime}
     </div>
   );
 });
