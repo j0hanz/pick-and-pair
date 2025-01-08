3e6de8f1646c3ae043224c75466d35af8c2eb21a
Linus Johansson <golfarenlj@gmail.com>
2025-01-08 12:18:45 +0000

    Rename Moves component to WrongMoves for improved clarity in game state

--- src/components/WrongMoves.jsx
+++ src/components/WrongMoves.jsx
@@ -1,11 +1,11 @@
 import React, { memo } from 'react';
 import { HiOutlineXMark } from 'react-icons/hi2';
-import styles from './styles/Moves.module.css';
+import styles from './styles/WrongMoves.module.css';
 
 const Moves = memo(({ moves }) => {
   return (
     <div className={styles.moves}>
-      <HiOutlineXMark className={`${styles.statsIcon} me-1`} /> {moves}
+      <HiOutlineXMark className="me-1" /> {moves}
     </div>
   );
 });
