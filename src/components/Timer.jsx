ad96db11141c380f4fb29e411fdd28e1985bd9bb
Linus Johansson <golfarenlj@gmail.com>
2025-01-09 21:14:03 +0000

    Adjust clock icon styling and spacing

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -8,7 +8,7 @@ const Timer = memo(() => {
 
   return (
     <div className={styles.timer}>
-      <HiOutlineClock className="me-1" /> {elapsedTime}
+      <HiOutlineClock className={`${styles.clockIcon} me-2`} /> {elapsedTime}
     </div>
   );
 });
