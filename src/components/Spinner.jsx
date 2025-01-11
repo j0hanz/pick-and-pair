4ad6fee0e6158b8421deb73880e8ebe6d14cae5d
Linus Johansson <golfarenlj@gmail.com>
2025-01-11 13:44:28 +0000

    Refactor spinner and styles for improved animation and design

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
 import styles from './styles/Spinner.module.css';
 
 const LoadingSpinner = ({ isLoading }) => (
   <div
     className={`${styles.spinnerContainer} ${!isLoading ? styles.hidden : ''}`}
   >
-    <div className={styles.spinnerBox}>
-      <Spinner animation="grow" role="status" className="mb-2"></Spinner>
-      <span className="opacity-75">Starting</span>
-    </div>
+    <div class={styles.loader}></div>
   </div>
 );
 
