36c41754f6053d0b360f598da4ca3b2f2d1f0b22
Linus Johansson <golfarenlj@gmail.com>
2025-01-14 11:14:48 +0000

    Fetch 5 latest commits

--- src/api/github.js
+++ src/api/github.js
@@ -4,21 +4,24 @@ const GITHUB_API_URL =
   'https://api.github.com/repos/j0hanz/pick-and-pair/commits';
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-export const fetchLatestCommit = async () => {
+// Fetches the latest commits from the repository
+export const fetchLatestCommits = async () => {
   try {
     const response = await axios.get(GITHUB_API_URL, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
+      params: {
+        per_page: 5, // Fetch 5 latest commits
+      },
     });
-    const latestCommit = response.data[0];
-    return {
-      message: latestCommit.commit.message,
-      date: latestCommit.commit.author.date,
-      url: latestCommit.html_url,
-    };
+    return response.data.map((commit) => ({
+      message: commit.commit.message,
+      date: commit.commit.author.date,
+      url: commit.html_url,
+    }));
   } catch (error) {
-    console.error('Error fetching latest commit:', error);
-    return null;
+    console.error('Error fetching latest commits:', error);
+    return [];
   }
 };
